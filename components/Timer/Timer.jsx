e06b440bff68b477eed80b1b0e28bbd47ed8946f
Faiz Hameed <faizftb@gmail.com>
2020-08-02 12:33:46 +0530

    added new logo for ticktivity

--- components/Timer/Timer.jsx
+++ components/Timer/Timer.jsx
@@ -4,7 +4,7 @@ import styles from "./Timer.module.scss";
 const Timer = () => {
   return (
     <div className={styles.timerCont}>
-      <h2 className={styles.timerText}>0.00.00</h2>
+      <h2 className={styles.timerText}>00:00:00</h2>
     </div>
   );
 };
