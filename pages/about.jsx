c87b9f0cbad4016988f72c6cf4b4d99ba811eb9d
Faiz Hameed <faizftb@gmail.com>
2020-08-04 18:59:22 +0530

    :art: Nav fixes and about page
    fixed css

---
+++ pages/about.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+import styles from "../styles/Home.module.css";
+
+const About = () => {
+  return (
+    <Layout>
+      {" "}
+      <main className={styles.main}>
+        <h1 className={styles.title}>About us</h1>
+
+        <p className={styles.description}>
+          We are a team of developers at elitiman
+        </p>
+      </main>
+    </Layout>
+  );
+};
+
+export default About;
