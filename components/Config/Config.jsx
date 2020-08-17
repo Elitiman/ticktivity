e81bcd602c79d5e77865e77b8be8f0be4a791ea1
Faiz Hameed <faizftb@gmail.com>
2020-08-17 15:14:48 +0530

    :fire: Rebuidling config

--- components/Config/Config.jsx
+++ components/Config/Config.jsx
@@ -3,11 +3,13 @@ import styles from "./Config.module.scss";
 
 const Config = () => {
   return (
-    <div className={styles.container}>
-      <label htmlFor="">Pomodoro Duration:</label>
-      <input type="number" />
-      <label htmlFor="">Break Duration:</label>
-      <input type="number" />
+    <div children={styles.wrapper}>
+      <div className={styles.container}>
+        <label htmlFor="">Pomodoro Duration:</label>
+        <input type="number" />
+        <label htmlFor="">Break Duration:</label>
+        <input type="number" />
+      </div>
     </div>
   );
 };
